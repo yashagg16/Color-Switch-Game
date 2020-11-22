03ee17a404453bbecded78d9449121750c4df4f8
shashank19107 <55681562+shashank19107@users.noreply.github.com>
2020-11-22 12:22:19 +0530

    Commiting Final demo for gamePlay

--- Code/src/sample/Gameplay.java
+++ Code/src/sample/Gameplay.java
@@ -1,21 +1,93 @@
 package sample;
 
-import javafx.application.Application;
+import javafx.animation.RotateTransition;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Arc;
+import javafx.scene.shape.Circle;
+import javafx.scene.transform.Rotate;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import java.io.IOException;
 
-public class Gameplay extends Application {
-
+public class Gameplay {
+    @FXML
+    private Pane Pane
+            ;
+    @FXML
+    private ImageView Star;
     @FXML
     private ImageView PauseButton_img;
+    @FXML
+    private Arc arc1;
+    @FXML
+    private Arc arc2;
+    @FXML
+    private Arc arc3;
+    @FXML
+    private Arc arc4;
+    @FXML
+    private Circle Incircle;
+
+    @FXML
+    public void initData(){
+        System.out.println("Working??");
+       final Group outerCircle = new Group(arc1, arc2, arc3, arc4,Incircle);
+       Group star = new Group(Star);
+        RotateTransition rotate = new RotateTransition();
+        RotateTransition rotate_anti = new RotateTransition();
+        rotate_anti.setAxis(Rotate.Z_AXIS);
+        rotate_anti.setFromAngle(360);
+        rotate_anti.setToAngle(0);
+        rotate.setAxis(Rotate.Z_AXIS);
+        rotate.setFromAngle(0);
+        rotate.setToAngle(360);
+        rotate_anti.setCycleCount(RotateTransition.INDEFINITE);
+        rotate.setCycleCount(RotateTransition.INDEFINITE);
+        rotate.setDuration(Duration.millis(1000));
+        rotate_anti.setDuration(Duration.millis(5000));
+        rotate.setNode(outerCircle);
+        rotate_anti.setNode(star);
+        rotate_anti.play();
+        rotate.play();
+       // Pane.
+
+        Pane.getChildren().add(outerCircle);
+        Pane.getChildren().add(star);
+//        RotateTransition rotate = new RotateTransition();
+//        rotate.setAxis(Rotate.Z_AXIS);
+//        rotate.setFromAngle(0);
+//        rotate.setToAngle(360);
+//        rotate.setCycleCount(1);
+//        rotate.setDuration(Duration.millis(1000));
+//        rotate.setNode((Node)outerCircle);
+//        rotate.play();
+//
+//
+   }
+   @FXML
+   public void flippause(MouseEvent event) throws  IOException {
+        flipIcon(PauseButton_img);
+   }
+   private void flipIcon(ImageView Icon){
+       RotateTransition rotate = new RotateTransition();
+       rotate.setAxis(Rotate.Z_AXIS);
+       rotate.setFromAngle(0);
+       rotate.setToAngle(360);
+       rotate.setCycleCount(1);
+       rotate.setDuration(Duration.millis(1000));
+       rotate.setNode((Node)Icon);
+       rotate.play();
+   }
+
     @FXML
     void jump(){
         System.out.println("Jump");
@@ -23,7 +95,7 @@ public class Gameplay extends Application {
 
     @FXML
     void openPauseMenu(MouseEvent event) throws IOException {
-        Parent Pause = FXMLLoader.load(getClass().getResource("PauseMenu.fxml"));
+        Parent Pause = FXMLLoader.load(getClass().getResource("pauseMenu.fxml"));
         Scene player = new Scene(Pause);
 
 
@@ -43,12 +115,5 @@ public class Gameplay extends Application {
 //            }
 //        }
 //    });
-    @Override
-    public void start(Stage PrimaryStage) throws IOException {
-        Parent StartMenu = FXMLLoader.load(getClass().getResource("Gameplay.fxml"));
-        final Scene scene = new Scene(StartMenu);
 
-        PrimaryStage.setScene(scene);
-        PrimaryStage.show();
-    }
 }
--- Code/src/sample/PauseMenu.java
+++ Code/src/sample/PauseMenu.java
@@ -24,12 +24,11 @@ public class PauseMenu  extends Application {
 
     @FXML
     void play(ActionEvent event) throws IOException {
-        Parent Enterplayername = FXMLLoader.load(getClass().getResource("Gameplay.fxml"));
-        Scene playername = new Scene(Enterplayername);
-
-        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
-        window.setScene(playername);
-        window.show();
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Gameplay.fxml"));
+        AnchorPane pane=fxmlLoader.load();
+        Gameplay controller = fxmlLoader.getController();
+        controller.initData();
+        GamePlayRoot.getChildren().setAll(pane);
         //GamePlayController controller = fxmlLoader.<GamePlayController>getController();
 
 
@@ -44,29 +43,31 @@ public class PauseMenu  extends Application {
     void exit(){
         System.out.println("Exit clicked");
     }
-
+    
     @FXML
     public void Enterplayernamescreen(ActionEvent event) throws IOException {
-        Parent Enterplayername = FXMLLoader.load(getClass().getResource("Gameplay.fxml"));
-        Scene playername = new Scene(Enterplayername);
-
-        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
-        window.setScene(playername);
-        window.show();
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Gameplay.fxml"));
+        AnchorPane pane=fxmlLoader.load();
+        Gameplay controller = fxmlLoader.getController();
+        controller.initData();
+        GamePlayRoot.getChildren().setAll(pane);
+//        Parent Enterplayername = FXMLLoader.load(getClass().getResource("Gameplay.fxml"));
+//        Scene playername = new Scene(Enterplayername);
+//
+//        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+//        window.setScene(playername);
+//        window.show();
 
     }
 
     @Override
     public void start(Stage PrimaryStage) throws IOException {
-        Parent StartMenu = FXMLLoader.load(getClass().getResource("PauseMenu.fxml"));
+        Parent StartMenu = FXMLLoader.load(getClass().getResource("pauseMenu.fxml"));
         PrimaryStage.setTitle("Color Switch");
         final Scene scene = new Scene(StartMenu);
+        //Gameplay game = new Gameplay();
 
         PrimaryStage.setScene(scene);
         PrimaryStage.show();
-    }
-
-    public static void main (String[] args){
-        launch(args);
     }
 }
